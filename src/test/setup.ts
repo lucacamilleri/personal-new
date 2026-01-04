52f5b45c753f4281aaa1860caa0c83418d63ea2d
lucacamilleri <095luca@gmail.com>
2026-01-04 10:04:19 +0100

    Set up testing environment with Vitest

---
+++ src/test/setup.ts
@@ -0,0 +1,30 @@
+﻿import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+// Cleanup after each test
+afterEach(() => {
+  cleanup();
+});
+
+// Mock localStorage
+const localStorageMock = {
+  getItem: (key) => {
+    return localStorageMock[key] || null;
+  },
+  setItem: (key, value) => {
+    localStorageMock[key] = value.toString();
+  },
+  removeItem: (key) => {
+    delete localStorageMock[key];
+  },
+  clear: () => {
+    Object.keys(localStorageMock).forEach((key) => {
+      if (key !== 'getItem' && key !== 'setItem' && key !== 'removeItem' && key !== 'clear') {
+        delete localStorageMock[key];
+      }
+    });
+  },
+};
+
+global.localStorage = localStorageMock;
---
+++ src/test/testUtils.tsx
@@ -0,0 +1,15 @@
+﻿import { configureStore } from '@reduxjs/toolkit';
+import themeReducer from '../store/slices/themeSlice';
+import projectsReducer from '../store/slices/projectsSlice';
+import contactReducer from '../store/slices/contactSlice';
+
+export const createTestStore = (preloadedState = {}) => {
+  return configureStore({
+    reducer: {
+      theme: themeReducer,
+      projects: projectsReducer,
+      contact: contactReducer,
+    },
+    preloadedState,
+  });
+};
